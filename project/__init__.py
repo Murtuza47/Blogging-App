b730d4b74c9c60049446aa34f71a0852fa9a695e
Ali Murtaza <a.murtuza007@gmail.com>
2021-01-15 10:50:35 +0500

    added

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,4 @@
+from project import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -7,10 +8,10 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRETKEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'a.murtuza007@gmail.com  '
-app.config['MAIL_PASSWORD'] = 'california2218287'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = True
 mail = Mail(app)
 bcrypt = Bcrypt(app)
@@ -18,5 +19,3 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = 'info'
 db = SQLAlchemy(app)
-
-from project import routes
